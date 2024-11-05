4a4f9509dfeb7c5b590b7ebfd47bc67e1471f252
Vladislav20052005 <149367170+Vladislav20052005@users.noreply.github.com>
2024-11-05 21:58:08 +0300

    Add files via upload

--- vector.h
+++ vector.h
@@ -1,29 +1,61 @@
 #include <cstddef>
-#include <stdecept>
 #include <memory>
+#include <stdexcept>
+#include <type_traits>
 #include <utility>
+
+#define MIN_RESERVED 8
+
+size_t AlignPow2(size_t x) {
+  size_t cnt = 0;
+  for (size_t i = 0; i < sizeof(size_t) * 8; i++) {
+    if ((1ULL << i) & x) {
+      cnt++;
+    }
+  }
+  if (cnt <= 1) {
+    return x;
+  }
+  for (size_t i = sizeof(size_t) * 8 - 1; i >= 0; i--) {
+    if ((1ULL << i) & x) {
+      return (1ULL << (i + 1));
+    }
+  }
+  return 0;
+}
 
 template <typename T> class Vector {
 public:
-  Vector() : size_(0), reserved_(8), data_(new T[8]) {}
-  Vector(size_t size, const T &t = T()) : size_(0), reserved_(8) {
-    data_ = new T[8];
+  Vector() : size_(0), reserved_(MIN_RESERVED), data_(new T[MIN_RESERVED]) {}
+  Vector(size_t size, T t = T()) : size_(size), reserved_(AlignPow2(size)) {
+    data_ = new T[reserved_];
     for (size_t i = 0; i < size; i++) {
-      PushBack(t);
+      data_[i] = t;
     }
   }
-  template <typename Iterator>
-  Vector(Iterator begin, Iterator end) : size_(0), reserved_(8) {
-    data_ = new T[8];
+  template <typename Iterator,
+            std::enable_if_t<!std::is_arithmetic<Iterator>::value> * = 0>
+  Vector(Iterator begin, Iterator end) {
+    size_t size = 0;
+    for (Iterator iter = begin; iter != end; ++iter)
+      size++;
+    size_ = size;
+    reserved_ = AlignPow2(size);
+    data_ = new T[reserved_];
+    size_t i = 0;
     for (Iterator iter = begin; iter != end; ++iter) {
-      PushBack(*iter);
+      data_[i] = *iter;
+      i++;
     }
   }
 
-  Vector(std::initializer_list<T> il) : size_(0), reserved_(8) {
-    data_ = new T[8];
-    for (T t : il) {
-      PushBack(t);
+  Vector(std::initializer_list<T> il)
+      : size_(il.size()), reserved_(AlignPow2(il.size())) {
+    data_ = new T[reserved_];
+    size_t i = 0;
+    for (const T &t : il) {
+      data_[i] = t;
+      i++;
     }
   }
   Vector(const Vector<T> &rhs) : size_(rhs.size_), reserved_(rhs.reserved_) {
@@ -32,16 +64,19 @@ public:
       data_[i] = rhs.data_[i];
     }
   }
-  Vector(Vector &&rhs) : size_(rhs.size_), reserved_(rhs.reserved_) {
+  Vector(Vector &&rhs)
+      : size_(0), reserved_(MIN_RESERVED), data_(new T[MIN_RESERVED]) {
     Swap(rhs);
   }
   ~Vector() {
-    delete[] data_;
-    data_ = nullptr;
+    if (data_ != nullptr) {
+      delete[] data_;
+      data_ = nullptr;
+    }
   }
 
   Vector<T> &operator=(const Vector<T> &rhs) {
-    delete[] data_;
+    this->~Vector();
     data_ = new T[rhs.reserved_];
 
     size_ = rhs.size_;
@@ -75,7 +110,7 @@ public:
     return data_[index];
   }
 
-  void PushBack(T t) {
+  void PushBack(T t = T()) {
     if (size_ == reserved_) {
       Reallocate();
     }
@@ -83,7 +118,13 @@ public:
     size_++;
   }
 
-  void PopBack() { size_--; }
+  T &&PopBack() {
+    if (size_ == 0) {
+      throw std::runtime_error("Trying to popback a vector with 0 size");
+    }
+    size_--;
+    return std::move(data_[size_ + 1]);
+  }
 
   void Swap(Vector<T> &rhs) {
     std::swap(data_, rhs.data_);
@@ -112,9 +153,9 @@ private:
   void Reallocate() {
     T *new_data = new T[reserved_ * 2];
     for (size_t i = 0; i < reserved_; i++) {
-      new_data[i] = data_[i];
+      new_data[i] = std::move(data_[i]);
     }
-    delete[] data_;
+    this->~Vector();
     std::swap(new_data, data_);
     reserved_ *= 2;
   }
